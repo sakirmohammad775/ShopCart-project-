36b062ae20a05c222d174f343f4bad2d22ee5bb3
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2025-12-21 23:43:10 +0600

    add appendchild

--- map.js
+++ map.js
@@ -1,3 +1,10 @@
 const doubled = [1, 2, 3].map(n => n * 2);
 
 console.log(doubled)
+
+const allBox=document.getElementsByClassName("box")
+for(let i=0;i<allBox.length;i++){
+    const element=allBox[i]
+    element.computedStyleMap.backgroundColor="green"
+    element.appendchild
+}
